d8e86dd3e7f116b7e183a0b6bcf6101b2f71971b
Noureddine-A <anoxybiosis@hotmail.com>
2023-07-06 16:41:18 +0200

    First commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+
+import Skeleton, { SkeletonTheme } from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
+
+import "./App.css";
+import Divider from "./assets/pattern-divider-desktop.svg";
 
 function App() {
+  const [advice, setAdvice] = useState({});
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    fetchAdvice();
+  }, []);
+
+  async function fetchAdvice() {
+    setLoading(true);
+    const response = await fetch("https://api.adviceslip.com/advice");
+    const data = await response.json();
+    setAdvice(data.slip);
+    setLoading(false);
+  }
+
+  console.log(loading);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app__container">
+      <div className="advice__container">
+        <SkeletonTheme baseColor="hsl(218, 23%, 16%)" highlightColor="#444">
+          <p>ADVICE #{advice.id}</p>
+          <div className="advice__content">
+            {loading ? <Skeleton count={3} /> : <h1>{advice.advice}</h1>}
+          </div>
+
+          <img src={Divider} alt={Divider} />
+          <div className="new__advice">
+            <button onClick={fetchAdvice}></button>
+          </div>
+        </SkeletonTheme>
+      </div>
     </div>
   );
 }
